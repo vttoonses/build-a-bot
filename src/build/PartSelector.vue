1d6aea97e72e6869444c6af7f41c1ca18983f324
Eugene Thompson <gene@baruconsulting.com>
2019-12-09 06:01:03 -0500

    Daily

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -27,6 +27,7 @@ export default {
     position: {
       type: String,
       required: true,
+      validator: value => ['top', 'left', 'right', 'bottom', 'center'].includes(value),
     },
   },
   data() {
@@ -37,18 +38,26 @@ export default {
       return this.parts[this.selectedPartIndex];
     },
   },
+  created() {
+    this.emitSelectedPart();
+  },
   methods: {
+    emitSelectedPart() {
+      this.$emit('partSelected', this.selectedPart);
+    },
     selectNextPart() {
       this.selectedPartIndex = getNextValidIndex(
         this.selectedPartIndex,
         this.parts.length,
       );
+      this.emitSelectedPart();
     },
     selectPreviousPart() {
       this.selectedPartIndex = getPreviousValidIndex(
         this.selectedPartIndex,
         this.parts.length,
       );
+      this.emitSelectedPart();
     },
 
   },
